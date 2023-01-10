334f6ae76bff58c9b443a9b6368b53194e34c420
Youssef Fakhry <yousseffakhry96@gmail.com>
2023-01-10 17:53:44 +0200

    First Commit

---
+++ backend/base/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
+from .models import Product
+from .serializer import ProductSerializer
+# Create your views here.
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    
+    routes=[
+        '/api/products',
+        '/api/products/create',
+        '/api/products/upload',
+        '/api/products/<id>/reviews',
+        
+    ]
+    return Response(routes)
+
+@api_view(['GET'])
+def getProducts(request):
+    products=Product.objects.all()
+    serializer=ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product= Product.objects.get(_id=pk)
+    serializer=ProductSerializer(product, many=False)   
+    return Response(serializer.data)
